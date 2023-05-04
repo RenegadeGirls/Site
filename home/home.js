04edc34681ef2ca3ae7fb97369bf9d51956d9b7e
99TheDark <99TheDark@gmail.com>
2023-05-03 21:09:13 -0700

    Mobile bar disabled + resizing updation

--- home/home.js
+++ home/home.js
@@ -1,3 +1,14 @@
-const content = document.getElementById("content");
+const resize = () => {
+    const content = document.getElementById("content");
+    content.style.height = `${innerHeight - content.offsetTop}px`;
+}
 
-content.style.height = `${innerHeight - content.offsetTop}px`;
+onload = () => {
+    resize();
+    onresize = () => resize();
+
+    if(!location.mobile) {
+        const pages = [...document.getElementsByClassName("page")];
+        pages.forEach(page => page.classList.add("pagebar"));
+    }
+}

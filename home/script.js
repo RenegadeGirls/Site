3655b6f36421529d0eb70b5d3782c30998f8ff9f
99TheDark <99TheDark@gmail.com>
2023-05-05 08:01:09 -0700

    Working on adding a more button + removed .html

--- home/script.js
+++ home/script.js
@@ -1,21 +1,24 @@
-const resize = () => {
-    const content = document.getElementById("content");
+const content = document.getElementById("content");
+const more = document.getElementById("more");
+
+onresize = () => {
     content.style.height = `${innerHeight - content.offsetTop}px`;
 };
+/*onscroll = () => {
+    more.style.opacity = `${(1.5 - scrollY / innerHeight * 2) * 100}%`;
+};*/
 
-onload = () => {
-    resize();
-    onresize = () => resize();
+onresize();
 
-    if(!location.mobile) {
-        const pages = [...document.getElementsByClassName("page")];
-        pages.shift();
-        pages.forEach(page => {
-            page.classList.add("pagebar");
+if(!location.mobile) {
+    const pages = [...document.getElementsByClassName("page")];
+    // Remove search bar & logo
+    pages.shift(), pages.pop();
 
-            let [icon, para] = page.children[0].children;
+    pages.forEach(page => {
+        page.classList.add("pagebar");
 
-            icon.title = para.textContent;
-        });
-    }
+        const [icon, para] = page.children[0].children;
+        icon.title = para.textContent;
+    });
 };

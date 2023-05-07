d2c51cf927982ab9c1d6f55682071de13d6f9bac
99TheDark <99TheDark@gmail.com>
2023-05-06 18:10:03 -0700

    Fixed navigation and linked pages

--- home/script.js
+++ home/script.js
@@ -5,17 +5,3 @@ onresize = () => {
 };
 
 onresize();
-
-if(!location.mobile) {
-    const pages = [...document.getElementsByClassName("page")];
-
-    // Remove search bar & logo
-    pages.shift(), pages.pop();
-
-    pages.forEach(page => {
-        page.classList.add("pagebar");
-
-        const [icon, para] = page.children[0].children;
-        icon.title = para.textContent;
-    });
-};

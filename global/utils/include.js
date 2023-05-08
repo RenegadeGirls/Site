7531fcc140dc1751c502d868994e8c25886f7183
99TheDark <99TheDark@gmail.com>
2023-05-08 09:19:16 -0700

    Fixed up navigation bar

--- global/utils/include.js
+++ global/utils/include.js
@@ -3,14 +3,23 @@ addEventListener("load", () => {
     elements.forEach(el => {
         if(el.hasAttribute("data-href")) {
             const href = el.getAttribute("data-href");
+            const parser = new DOMParser();
             fetch(href)
                 .then(html => html.text())
                 .then(text => {
-                    const parser = new DOMParser();
-                    const doc = parser.parseFromString(text, "text/html");
-                    el.parentElement.replaceChild(doc.body.children[0], el);
+                    const children = [...parser.parseFromString(text, "text/html").body.children];
+
+                    children.forEach(child => {
+                        // Is there a better way to do this that makes the scripts reload?
+                        const copy = document.createElement(child.tagName);
+                        copy.innerHTML = child.innerHTML;
+                        [...child.attributes].forEach(attr => copy.setAttribute(attr.name, attr.value));
+
+                        el.parentNode.insertBefore(copy, el);
+                    });
                 })
-                .catch(error => console.error(`Could not find ${href}`))
+                .catch(e => console.error(e))
+                .catch(() => console.error(`Could not find ${href}`))
         }
     });
 });
--- global/utils/nav.js
+++ global/utils/nav.js
@@ -1,15 +1,13 @@
-addEventListener("load", () => {
-    if(!location.mobile) {
-        const pages = [...document.getElementsByClassName("page")];
+if(!location.mobile) {
+    const pages = [...document.getElementsByClassName("page")];
 
-        // Remove search bar & logo
-        pages.shift(), pages.pop();
+    // Remove search bar & logo
+    pages.shift(), pages.pop();
 
-        pages.forEach(page => {
-            page.classList.add("pagebar");
+    pages.forEach(page => {
+        page.classList.add("pagebar");
 
-            const [icon, para] = page.children[0].children;
-            icon.title = para.textContent;
-        });
-    };
-});
+        const [icon, para] = page.children[0].children;
+        icon.title = para.textContent;
+    });
+};

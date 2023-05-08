d8b3306efb9b2265617622854a4dc2a719d3a0da
99TheDark <99TheDark@gmail.com>
2023-05-07 22:07:58 -0700

    Added tooltips and moved nav script

--- global/utils/nav.js
+++ global/utils/nav.js
@@ -1,13 +1,15 @@
-if(!location.mobile) {
-    const pages = [...document.getElementsByClassName("page")];
+addEventListener("load", () => {
+    if(!location.mobile) {
+        const pages = [...document.getElementsByClassName("page")];
 
-    // Remove search bar & logo
-    pages.shift(), pages.pop();
+        // Remove search bar & logo
+        pages.shift(), pages.pop();
 
-    pages.forEach(page => {
-        page.classList.add("pagebar");
+        pages.forEach(page => {
+            page.classList.add("pagebar");
 
-        const [icon, para] = page.children[0].children;
-        icon.title = para.textContent;
-    });
-};
+            const [icon, para] = page.children[0].children;
+            icon.title = para.textContent;
+        });
+    };
+});

185111770faf5efe2ddea651e5952579429041cf
99TheDark <99TheDark@gmail.com>
2023-06-03 12:50:50 -0700

    Reformatted

--- home/script.js
+++ home/script.js
@@ -4,5 +4,5 @@ const resizeDots = () => {
     topContent.style.height = `${innerHeight - topContent.offsetTop}px`;
 };
 
-ready(() => resizeDots());
-addEventListener("resize", () => resizeDots());
+ready(resizeDots);
+addEventListener("resize", resizeDots);
